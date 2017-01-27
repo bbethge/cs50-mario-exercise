621bc5afafbf1e9e9beb43ce222c890f6495347a
Ben Bethge <bethge931@gmail.com>
2017-01-27 14:58:06 -0600

    Implemented file output

--- com/example/mario/Mario.java
+++ com/example/mario/Mario.java
@@ -5,20 +5,74 @@ package com.example.mario;
 import java.lang.String;
 import java.lang.Integer;
 import java.io.Console;
+import java.io.IOException;
+import java.io.Writer;
+import java.io.FileWriter;
+import java.io.OutputStreamWriter;
 
 class Mario {
     public static void main(String[] args) {
-        int h;
-        h = getInt("Height? ", 0, 23);
-        for (int w = 2; w <= h+1; w++) {
-            for (int i = 0; i < h+1-w; i++) {
-                System.out.print(' ');
+        Writer writer = null;
+        boolean toFile = getBool("Output to file (yes/no)? ");
+        if (toFile) {
+            do {
+                System.out.print("File name? ");
+                Console c = System.console();
+                String file = c.readLine();
+                try {
+                    writer = new FileWriter(file);
+                }
+                catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
             }
-            for (int i = 0; i < w; i++) {
-                System.out.print('#');
-            }
-            System.out.println();
+            while (writer == null);
         }
+        else {
+            writer = new OutputStreamWriter(System.out);
+        }
+        int h = getInt("Height (0–23)? ", 0, 23);
+        try {
+            for (int w = 2; w <= h+1; w++) {
+                for (int i = 0; i < h+1-w; i++) {
+                    writer.write(" ");
+                }
+                for (int i = 0; i < w; i++) {
+                    writer.write("#");
+                }
+                writer.write(System.lineSeparator());
+            }
+            writer.flush();
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        if (toFile) {
+            try {
+                writer.close();
+            }
+            catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static boolean getBool(String prompt) {
+        Console c = System.console();
+        boolean valid = false;
+        boolean ret = false;
+        while (!valid) {
+            System.out.print(prompt);
+            String boolStr = c.readLine().toLowerCase();
+            if (boolStr.equals("y") || boolStr.equals("yes")) {
+                ret = true;
+                valid = true;
+            }
+            else if (boolStr.equals("n") || boolStr.equals("no")) {
+                valid = true;
+            }
+        }
+        return ret;
     }
 
     private static int getInt(String prompt, int min, int max) {
